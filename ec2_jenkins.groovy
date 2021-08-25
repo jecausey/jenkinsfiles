2c29a88e1eadda9401140060dd50bfb8202040be
Jamie Causey <jcausey@Jamies-MacBook-Air.local>
2021-08-24 20:50:06 -0500

    addding

--- ec2_jenkins.groovy
+++ ec2_jenkins.groovy
@@ -4,37 +4,37 @@ pipeline {
     stages {
         stage('Checkout') {
             steps {
-            checkout([$class: 'GitSCM', branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/jecausey/ec2_instances.git']]])
+            /* groovylint-disable-next-line LineLength */
+            checkout([$class: 'GitSCM', branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/jecausey/ec2.git']]])
             }
         }
-   
+
         stage ('terraform init') {
             steps {
+                /* groovylint-disable-next-line DuplicateStringLiteral */
                 sh ('terraform init')
             }
         }
 
         stage ('terraform Action') {
             steps {
-
+                /* groovylint-disable-next-line LineLength */
                 script { properties([parameters([choice(choices: ['apply', 'destroy'], description: 'Choose Apply or Destroy?', name: 'action')])])
-                    }   
+                }
 
                 withCredentials([[
                 $class: 'AmazonWebServicesCredentialsBinding',
-                credentialsId: "jenkins_terraform",
+                credentialsId: 'jenkins_terraform',
                 accessKeyVariable: 'AWS_ACCESS_KEY_ID',
                 secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'
                 ]])
 
-            
                 {
                 echo "Terraform action is --> ${action}"
                 /* groovylint-disable-next-line GStringExpressionWithinString */
                 sh ('terraform ${action} --auto-approve')
+                }
             }
         }
     }
 }
-}
-
